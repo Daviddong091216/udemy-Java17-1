46a01525a4dda6e67f3b98ea15bb9fd2f4260947
daviddong091216 <daviddong201712@gmail.com>
2023-07-28 10:15:20 -0500

    20230728

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.print("Hello world!");
+        int myFirstNumber = 7;
+        System.out.print(myFirstNumber);
     }
 }
